a4cbeecd474c65aef4b3258a56b47a27b78b13fe
Taranveer Singh <taranveersingh93@gmail.com>
2023-08-07 11:20:17 -0400

    Add key to iterated components

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -2,14 +2,13 @@ import React from "react";
 import "./Orders.css";
 
 const Orders = ({orders}) => {
-  console.log(orders)
   const orderEls = orders.map(order => {
     return (
-      <div className="order">
+      <div id={order.id} className="order" key={order.id}>
         <h3>{order.name}</h3>
         <ul className="ingredient-list">
-          {order.ingredients.map((ingredient) => {
-            return <li>{ingredient}</li>;
+          {order.ingredients.map((ingredient, index) => {
+            return <li key={index+1}>{ingredient}</li>;
           })}
         </ul>
       </div>
